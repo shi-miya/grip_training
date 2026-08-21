e2fdfda32b5ea01228b595eb24fa3b8f75a138bd
shi-miya <60633936+shi-miya@users.noreply.github.com>
2026-08-21 23:12:01 +0900

    Add files via upload

---
+++ sw 2.js
@@ -0,0 +1,1 @@
+const C="grip-training-v4";const A=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
